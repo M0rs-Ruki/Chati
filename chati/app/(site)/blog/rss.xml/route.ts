46daf7c59d5cd75438e15b144e728aa61f2bb2dc
M0rs-Ruki <anuppradhan929@gmail.com>
2025-11-13 22:20:32 +0530

    feat: add content namespace to RSS feed for enhanced compatibility

--- chati/app/(site)/blog/rss.xml/route.ts
+++ chati/app/(site)/blog/rss.xml/route.ts
@@ -25,7 +25,9 @@ export async function GET() {
   const posts = await getBlogPosts()
 
   const rss = `<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<rss version="2.0" 
+     xmlns:atom="http://www.w3.org/2005/Atom"
+     xmlns:content="http://purl.org/rss/1.0/modules/content/">
   <channel>
     <title>Chati Blog</title>
     <link>${baseUrl}/blog</link>
